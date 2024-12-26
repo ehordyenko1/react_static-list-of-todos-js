b8d6733162149a8c3aae0dfda3eb07714a2fd505
Yehor Gordiienko <egordienko319@gmail.com>
2024-12-26 19:13:23 +0300

    test

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => {
+  return (
+    <div className="UserInfo">
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
